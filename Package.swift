efa493f703264040909754b3b6f31928824e8b67
Adam Fowler <adamfowler71@gmail.com>
2019-10-15 10:27:33 +0100

    Update to v3.3.0

--- Package.swift
+++ Package.swift
@@ -1,16 +1,16 @@
-// swift-tools-version:4.0
+// swift-tools-version:4.2
 
 import PackageDescription
 
 let package = Package(
-  name: "SwiftAWSEcr",
+  name: "ECR",
   products: [
-      .library(name: "SwiftAWSEcr", targets: ["SwiftAWSEcr"]),
+      .library(name: "ECR", targets: ["ECR"]),
   ],
   dependencies: [
-      .package(url: "https://github.com/noppoMan/aws-sdk-swift-core.git", .upToNextMajor(from: "1.0.0"))
+      .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .upToNextMinor(from: "3.4.0"))
   ],
   targets: [
-      .target(name: "SwiftAWSEcr", dependencies: ["AWSSDKSwiftCore"]),
+      .target(name: "ECR", dependencies: ["AWSSDKSwiftCore"]),
   ]
 )
