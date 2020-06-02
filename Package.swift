0cb41db88dd32e4b1d662144631b9d0f21baffe3
Adam Fowler <adamfowler71@gmail.com>
2020-06-02 16:51:23 +0100

    Version 4.7.0

--- Package.swift
+++ Package.swift
@@ -1,16 +1,16 @@
 // swift-tools-version:5.0
-
 import PackageDescription
 
 let package = Package(
-  name: "ECR",
-  products: [
-      .library(name: "ECR", targets: ["ECR"]),
-  ],
-  dependencies: [
-      .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .upToNextMinor(from: "4.2.0"))
-  ],
-  targets: [
-      .target(name: "ECR", dependencies: ["AWSSDKSwiftCore"]),
-  ]
+    name: "ECR",
+    platforms: [.iOS(.v12), .tvOS(.v12), .watchOS(.v5)],
+    products: [
+        .library(name: "ECR", targets: ["ECR"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/swift-aws/aws-sdk-swift-core.git", .upToNextMinor(from: "4.6.0"))
+    ],
+    targets: [
+        .target(name: "ECR", dependencies: ["AWSSDKSwiftCore"], path: "./Sources/ECR"),
+    ]
 )
